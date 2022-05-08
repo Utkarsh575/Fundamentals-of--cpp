4635d2139055f35539c0ce4226d97f8c4ca21732
utkarsh575 <utkarshlohani575@gmail.com>
2022-05-08 22:07:48 +0530

    :sparkles: added queen , sugarcane

---
+++ basics/sugarcane.cpp
@@ -0,0 +1,21 @@
+// submission of may long Sugarcane Juice Business by shatirbillu575
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,profit;
+        cin>>n;
+        if(n>0){
+            profit=n*(0.3*50);
+            cout<<profit<<endl;
+
+        }
+        else{cout<<0;}
+    }
+
+
+    return 0;
+}
